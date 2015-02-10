e9c2daeb73312ace479f516d5d30404478268a4f
Ian Jackson <ian.jackson@eu.citrix.com>
2015-02-10 19:37:33 +0000

    libxl: ao: datacopier callback gets an rc
    libxl__datacopier_* now provides its caller's callback function with
    an rc value.  This relieves the caller of the need to figure out an
    appropriate rc value.
    
    Arrange that the `other internal failure' cases now get a valid
    positive errno value (EIO).
    
    In a few places, assert that errno is nonzero before passing it to our
    caller.
    
    Extend the datacopier callback API to permit the dc to signal
    CANCELLED.  (It doesn't actually do this yet, though.)
    
    Signed-off-by: Ian Jackson <ian.jackson@eu.citrix.com>
    ---
    v2: New in this version of the series.

--- tools/libxl/libxl_dom.c
+++ tools/libxl/libxl_dom.c
@@ -1918,7 +1918,7 @@ out:
 }
 
 static void save_device_model_datacopier_done(libxl__egc *egc,
-     libxl__datacopier_state *dc, int onwrite, int errnoval);
+     libxl__datacopier_state *dc, int rc, int onwrite, int errnoval);
 
 void libxl__domain_save_device_model(libxl__egc *egc,
                                      libxl__domain_suspend_state *dss,
@@ -1977,11 +1977,11 @@ void libxl__domain_save_device_model(libxl__egc *egc,
     return;
 
  out:
-    save_device_model_datacopier_done(egc, dc, -1, 0);
+    save_device_model_datacopier_done(egc, dc, rc, -1, EIO);
 }
 
 static void save_device_model_datacopier_done(libxl__egc *egc,
-     libxl__datacopier_state *dc, int onwrite, int errnoval)
+     libxl__datacopier_state *dc, int our_rc, int onwrite, int errnoval)
 {
     libxl__domain_suspend_state *dss =
         CONTAINER_OF(dc, *dss, save_dm_datacopier);
@@ -1989,13 +1989,9 @@ static void save_device_model_datacopier_done(libxl__egc *egc,
 
     /* Convenience aliases */
     const char *const filename = dss->dm_savefile;
-    int our_rc = 0;
     int rc;
 
     libxl__datacopier_kill(dc);
-
-    if (onwrite || errnoval)
-        our_rc = ERROR_FAIL;
 
     if (dc->readfd >= 0) {
         close(dc->readfd);
