90a929d3cbea8f8afa157f836a08a3ab122efcca
Ian Jackson <ian.jackson@eu.citrix.com>
2015-02-10 19:00:06 +0000

    libxl: Introduce FILLZERO
    FILLZERO is a macro for memset(&foo,0,sizeof(foo)).  It eliminates the
    possiblity to make the error memset(&foo,0,sizeof(&foo)).
    
    No callers yet, but document it in CODING_STYLE.  (In accordance with
    existing libxl policy, I haven't gone through all existing possible
    call sites.)
    
    Signed-off-by: Ian Jackson <Ian.Jackson@eu.citrix.com>
    ---
    v2: New in this version of the series.

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -3225,6 +3225,9 @@ _hidden const char *libxl__device_model_savefile(libxl__gc *gc, uint32_t domid);
     })
 
 
+#define FILLZERO LIBXL_FILLZERO
+
+
 /*
  * All of these assume (or define)
  *    libxl__gc *gc;
